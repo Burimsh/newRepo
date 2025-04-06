d21bb7f599737b5339b9fbc57adaff58911bb790
Burimsh <Burimsh.sm@gmail.com>
2025-04-06 15:19:30 +0200

    ..

--- LAB4/scripts/backup.sh
+++ LAB4/scripts/backup.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+# Variables
+DB_CONTAINER="lab4-db-1"
+
 # Create backup directory if it doesn't exist
 mkdir -p ./backups
 
 # Perform MySQL database backup
-docker exec db mysqldump -u user -ppassword api_db > ./backups/db_backup_$(date +"%Y%m%d_%H%M%S").sql
+docker exec $DB_CONTAINER mysqldump -u user -ppassword api_db > ./backups/db_backup_$(date +"%Y%m%d_%H%M%S").sql
 
 # Check if the backup was successful
 if [ $? -eq 0 ]; then
-    echo "Backup completed successfully"
+    echo "Backup completed successfully: ./backups/db_backup_$(date +"%Y%m%d_%H%M%S").sql"
 else
     echo "Backup failed!"
     exit 1
